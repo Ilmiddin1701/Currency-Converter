9fbc7800e32762441fbb93670898ef7fdeabcc42
ilmid <ilmiddinorinov1@gmail.com>
2024-06-04 19:38:35 +0500

    currency converter

--- app/src/main/java/com/example/valutaconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/valutaconverterapp/MainActivity.kt
@@ -25,6 +25,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rvAdapter: RvAdapter
     private lateinit var list: ArrayList<Courses>
     private lateinit var requestQueue: RequestQueue
+    var p = 1
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -32,36 +34,62 @@ class MainActivity : AppCompatActivity() {
         requestQueue = Volley.newRequestQueue(this)
         val url = "https://cbu.uz/uzc/arkhiv-kursov-valyut/json/"
         val jsonArrayRequest =
-            JsonArrayRequest(Request.Method.GET, url, null, object : Response.Listener<JSONArray> {
-                override fun onResponse(response: JSONArray?) {
-                    val str = response.toString()
-                    val type = object : TypeToken<ArrayList<Courses>>() {}.type
-                    list = Gson().fromJson(str, type)
-                    rvAdapter = RvAdapter(list)
-                    binding.rv.adapter = rvAdapter
-
-                    val spinnerList = ArrayList<Spinner>()
-                    list.forEach {
-                        spinnerList.add(Spinner(it.Ccy))
-                    }
-                    binding.apply {
-                        spinnerFrom.adapter = SpinnerAdapter(spinnerList)
-                        spinnerFrom.onItemSelectedListener = object :
-                            AdapterView.OnItemSelectedListener {
-                            @SuppressLint("SetTextI18n")
-                            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                                tvRN.text = "1 ${list[position].Ccy} = ${list[position].Rate} so'm"
+            JsonArrayRequest(Request.Method.GET, url, null, { response ->
+                val str = response.toString()
+                val type = object : TypeToken<ArrayList<Courses>>() {}.type
+                list = Gson().fromJson(str, type)
+                list.add(Courses(0, "Salom", "UZS", "UZS", "UZS", "UZS", "UZS", "1000", "1000", "1", list[1].Date))
+                rvAdapter = RvAdapter(list)
+                binding.rv.adapter = rvAdapter
+                val spinnerList = ArrayList<Spinner>()
+                var from = 0f
+                var to = 0f
+                list.forEach {
+                    spinnerList.add(Spinner(it.Ccy))
+                }
+                binding.apply {
+                    spinnerFrom.adapter = SpinnerAdapter(spinnerList)
+                    spinnerFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                        @SuppressLint("SetTextI18n")
+                        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                            if (list[position].id != 0){
                                 tvDate.text = list[position].Date
+                                tvRN.text = "1 ${list[position].Ccy} = ${list[position].Rate} so'm"
+                                from = list[position].Rate.toFloat()
+                                tvName.text = "${list[position].CcyNm_UZ} (${list[position].Ccy})"
+                            } else {
+                                p = list[position].id
                                 tvName.text = "${list[position].CcyNm_UZ} (${list[position].Ccy})"
                             }
-
-                            override fun onNothingSelected(parent: AdapterView<*>?) {}
+                        }
+                        override fun onNothingSelected(parent: AdapterView<*>?) {}
+                    }
+                    spinnerTo.adapter = SpinnerAdapter(spinnerList)
+                    spinnerTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                        @SuppressLint("SetTextI18n")
+                        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                            tvDate1.text = list[position].Date
+                            tvRN1.text = "1 ${list[position].Ccy} = ${list[position].Rate} so'm"
+                            to = list[position].Rate.toFloat()
+                            tvName1.text = "${list[position].CcyNm_UZ} (${list[position].Ccy})"
+                        }
+                        override fun onNothingSelected(parent: AdapterView<*>?) {}
+                    }
+                    btnConvert.setOnClickListener {
+                        if (edtAmount.text.isNotEmpty()) {
+                            if (p != 0){
+                                val c = from / to
+                                val result = (c * edtAmount.text.toString().toFloat())
+                                tvResult.text = "%.3f".format(result)
+                            }
+                        } else {
+                            Toast.makeText(this@MainActivity, "Enter the amount!", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
-            }, Response.ErrorListener {
-                Toast.makeText(this, "Server xatoligi", Toast.LENGTH_SHORT).show()
-            })
+            }) {
+                Toast.makeText(this, "No Internet connection. Please try again!", Toast.LENGTH_SHORT).show()
+            }
         requestQueue.add(jsonArrayRequest)
     }
 }
